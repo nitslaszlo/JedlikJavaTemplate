4ec52d18c0d9edd4d2cd4984eabbe2c70395bbe6
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2017-12-16 19:52:00 +0100

    Up

--- src/main/java/eu/jedlik/JedlikJavaTemplate/App.java
+++ src/main/java/eu/jedlik/JedlikJavaTemplate/App.java
@@ -9,7 +9,7 @@ public class App
         System.out.println("Hello World!");
         System.out.println("árvíztűrő tükörfúrógép - ÁRVÍZTŰRŐ TÜKÖRFÚRÓGÉP");
         System.out.print("Kérem a nevét: ");
-        Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in, "Cp852");
         System.out.println("Hello " + s.nextLine());
         s.close();
     }
